2af660a06abdf029d5fb3e6dc8457b883a137197
yevgeniyT <yevgentolkachov@gmail.com>
2023-08-08 22:20:02 +0200

    EB config-3

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(apiContentType);
 
 // healty check
-app.get("/api/v1/health", (req, res) => {
+app.get("/", (req, res) => {
     res.status(200).send({ status: "ok" });
 });
 
